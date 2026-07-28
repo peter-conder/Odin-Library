1e87b3b26fea329b96c45e99a7eaf20f31c7d532
peter-conder <pconder2005@gmail.com>
2026-07-28 16:23:08 -0600

    add index.html, styles.css, script.js, add book object

---
+++ script.js
@@ -0,0 +1,36 @@
+/** Pseudocode
+ * Make book constructor
+ * Store books in array
+ * Function
+     * create a book from arguments
+     * store book in array
+ * Every book should have a unique id
+ * 
+ * 
+ *
+ *  
+ * */
+
+
+const library = [];
+
+function Book(title, author, pages, read, identifier) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.identifier = self.crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read, id) {
+    const book = new Book(title, author, pages, read, id);
+    library.push(book);
+}
+
+addBookToLibrary("thing", "owjncw", "pages", "read", "id");
+addBookToLibrary("thing", "owjncw", "pages", "read", "id");
+console.log(library);
+
+for (const property in library.1) {
+    console.log(`${property}: ${object[property]}`);
+}
