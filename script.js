ebbfb7e2c48b1c0704f70340a9ad75b85035a476
peter-conder <pconder2005@gmail.com>
2026-07-31 09:50:27 -0600

    add form, form shows on click, add styling

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const library = [];
 
-Book.prototype
+/*
+const bookPrototype = {
+    title: title,
+    author: author,
+    pages: pages,
+    read: false
+};
+*/
 
 function Book(title, author, pages, read, identifier) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
     this.identifier = self.crypto.randomUUID();
 }
 
@@ -14,22 +21,6 @@ function addBookToLibrary(title, author, pages) {
     const book = new Book(title, author, pages);
     library.push(book);
 }
-
-/*
-addBookToLibrary("book1", "you", "12312", "false", "id");
-addBookToLibrary("book2", "owjncw", "6767", "false", "id");
-addBookToLibrary("book3", "me", "1812", "true", "id");
-addBookToLibrary("book1", "you", "12312", "false", "id");
-addBookToLibrary("book2", "owjncw", "6767", "false", "id");
-addBookToLibrary("book3", "me", "1812", "true", "id");
-*/
-
-//console.log(library);
-
-/**
- * Remove all of existing elements from page, reprint library[]
- */
-
 
 function displayBooks() {
     let i = 0;
@@ -48,39 +39,25 @@ function displayBooks() {
     };
     };
 
-/** Create form onclick:
- * Instead of form, what if instead a bunch of prompts popped up? asking for name, author, pages, then adding all that to the DOM
- * */
 
-const button = document.getElementById("addButton");
+const addButton = document.getElementById("addButton");
+const form = document.getElementById("bookForm");
+//button shows/dismisses form
+addButton.addEventListener("click", () => {
+    if (form.style.display === 'none') {
+        form.style.display = 'flex';
+    } else {
+        form.style.display = 'none';
+    }
+});
 
-button.addEventListener('click', () => {
-    let bookTitle = prompt("What is your book called?");
-    let bookAuthor = prompt("Who wrote this book?");
-    let bookPages = prompt("how many pages are there in this book?");
-    addBookToLibrary(bookTitle, bookAuthor, bookPages);
-    displayBooks();
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('submit', () => {
+    event.preventDefault();
+    alert('it works');
+    let bookTitle = document.getElementById('bookTitle').value;
+    let bookAuthor = document.getElementById('author').value;
+    let bookPages = document.getElementById('pages').value;
+
+    console.log(bookTitle);
 })
-/*const form = document.getElementById("bookForm")
-button.addEventListener("click", () => {
-    if (form.style.display === "none") {
-        form.style.display = 'flex';}
-    else form.style.display = 'none';
-    });
-
-let bookTitle = document.getElementById("bookTitle").value;
-console.log(bookTitle);
-
-/*
-const submit = document.getElementById("submit");
-function submitForm () {
-    let bookTitle = document.getElementById("bookTitle").value;
-    let bookAuthor = document.getElementById("author").value;
-    let bookPages = document.getElementById("pages").value;
-    addBookToLibrary(bookTitle, bookAuthor, bookPages);
-    event.preventDefault;
-}
-
-submit.addEventListener('submit', submitForm());
-*/
-
